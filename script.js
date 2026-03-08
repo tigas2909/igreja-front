9b6d925df6a3452725f59aa1ea3733c03e5c5f77
tigas2909 <t47801375@gmail.com>
2026-03-08 00:26:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const logo = document.getElementById("logoMenu");
+const menu = document.getElementById("menu");
+
+logo.addEventListener("click", function(){
+
+menu.classList.toggle("menu-escondido");
+
+});
